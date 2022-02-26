94e5d4de3d0b764f5d8fa5de970cdc586b1f2f3b
민재 김 <gimm5829@gmail.com>
2022-02-26 20:56:52 +0900

    kkkl

---
+++ ConsoleApp8/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your age:");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            string a = age.ToString();
+
+            Console.WriteLine("Your age is: " + a);
+
+
+            Console.ReadLine();
+        }
+    }
+}
